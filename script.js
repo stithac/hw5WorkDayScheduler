be823d761e68ab59312155a3e1868acfa3072b15
Ashley Stith <ashleyc.stith@gmail.com>
2020-12-12 22:43:02 -0500

    Initial push

---
+++ script.js
@@ -0,0 +1,119 @@
+//Global variables
+var startHour = 9;
+times = "9 10 11 12 1 2 3 4 5".split(" ");
+j = 0;
+h = 9;
+var hourEl;
+
+var eventMoment = moment();
+
+console.log(eventMoment);
+console.log(eventMoment.format("hA"));
+
+console.log(eventMoment.format("MMMM D YYYY hA"));
+
+var currentYear = eventMoment.format("YYYY");
+var currentMonth = eventMoment.format("MMMM");
+var currentDay = eventMoment.format("D");
+var currentHour = eventMoment.format("HH");
+
+var meetings = [];
+
+//function builds out the actual scheduler page with the times/descriptions
+function buildScheduler(){
+
+    for(i = 0; i < 9; i++){
+
+        var timeblockDiv = $("<div>");
+        timeblockDiv.attr("class", "time-block");
+        $(".container").append(timeblockDiv);
+
+        var hourDiv = $("<div>");
+        hourDiv.attr("class", "hour");
+        hourDiv.attr("data-hour", h);
+
+
+        timeblockDiv.append(hourDiv);
+
+        if(j < 3 ){
+            hourEl = times[i] + " AM";
+        }else{
+            hourEl = times[i] + " PM";
+        }
+
+        $(hourDiv).text(hourEl);
+        console.log($(hourDiv).text());
+
+        var descriptionEl = $("<input>");
+        descriptionEl.attr("class", "description");
+        descriptionEl.attr("data-hour", h);
+        descriptionEl.attr("id", h);
+
+        console.log(currentHour);
+        console.log($(hourDiv).attr("data-hour"));
+
+        if(parseInt($(hourDiv).attr("data-hour")) < currentHour){
+            $(descriptionEl).attr("class", "description past");
+            console.log("Past");
+        } else if($(hourDiv).attr("data-hour") == currentHour){
+            $(descriptionEl).attr("class", "description present");
+            console.log("Current Hour!");
+        } else{
+            $(descriptionEl).attr("class", "description future");
+        }
+
+        timeblockDiv.append(descriptionEl);
+
+        var saveDiv = $("<div>");
+        saveDiv.attr("class", "save");
+        $(timeblockDiv).append(saveDiv);
+
+        var saveBtn = $("<input>");
+        saveBtn.attr("type", "submit");
+        saveBtn.attr("class", "saveBtn");
+        saveBtn.attr("data-hour", h);
+        $(saveDiv).append(saveBtn);
+
+        startHour++;
+        j++;
+        h++;
+    }
+}
+
+function saveInput(btn){
+
+    //check to see if there are any storedDescription in localStorage. If so, set them to the empty descriptions array
+    var storedMeetings = JSON.parse(localStorage.getItem("meetings"));
+    if (storedMeetings !== null){
+        meetings = storedMeetings;
+    }
+    console.log("Save Button");
+    console.log(btn);
+
+    var dataHour = $(btn).attr("data-hour");
+
+    // var des = $(".description[data-hour = dataHour]").text();
+
+    var eventData = {
+        hour: dataHour,
+        description: $("#" + dataHour).val()
+    }
+
+        meetings.push(eventData);
+
+
+    localStorage.setItem("meetings", JSON.stringify(meetings));
+}
+
+$(document).on("click", ".saveBtn", function(event){
+
+    event.preventDefault();
+    console.log("Save Button");
+
+    saveInput($(this));
+
+});
+
+buildScheduler();
+
+
