e2ba2335a8af413f2d4b6124f7cc40aba275183f
Ashley Stith <ashleyc.stith@gmail.com>
2020-12-14 11:42:28 -0500

    Updating look and feel

--- script.js
+++ script.js
@@ -1,86 +1,82 @@
 //Global variables
-var startHour = 9;
-times = "9 10 11 12 1 2 3 4 5".split(" ");
-j = 0;
-h = 9;
-var hourEl;
-
+var times = "9 10 11 12 1 2 3 4 5".split(" ");
 var eventMoment = moment();
-
-console.log(eventMoment);
-
-console.log(eventMoment.format("MMMM D YYYY hA"));
-
+    console.log(eventMoment); //Console log current date and time
 var currentYear = eventMoment.format("YYYY");
 var currentMonth = eventMoment.format("MMMM");
-var currentDay = eventMoment.format("D");
+var currentDay = eventMoment.format("MMMM, D YYYY");
 var currentHour = parseInt(eventMoment.format("HH"), 10);
+    // console.log(eventMoment.format("HH"));
+    // console.log("Current hour: " + currentHour);
 
-console.log(eventMoment.format("HH"));
-console.log("Current hour: " + currentHour);
+var meetings = []; //Creates empty meetings array
 
-
-var meetings = [];
-
-//function builds out the actual scheduler page with the times/descriptions
+//Function buildScheduler() is called on page load
+//builds out the actual scheduler page with the times/descriptions
 function buildScheduler(){
+    var j = 0;
+    var h = 9;
+    var hourEl;
+
+    $(".imgGif").attr("src", "https://media.giphy.com/media/iH6ZjzyLgZSt3j0zzG/giphy.gif");
+
+    $("#currentDay").text(currentDay);
 
     for(i = 0; i < 9; i++){
 
+        //Create timeblock divs on page:
+
+        //timeblock divs
         var timeblockDiv = $("<div>");
         timeblockDiv.attr("class", "time-block");
         $(".container").append(timeblockDiv);
 
+        //hour elements
         var hourDiv = $("<div>");
-        hourDiv.attr("class", "hour");
+        hourDiv.attr("class", "hour smallEl");
         hourDiv.attr("data-hour", parseInt(h, 10));
         hourDiv.attr("id", h + "hour");
 
-        timeblockDiv.append(hourDiv);
+        timeblockDiv.append(hourDiv); //append hour to timeblock div
 
+        //Add AM for the first 3 hour elements then PM to the rest. Times are stored in the times array
         if(j < 3 ){
             hourEl = times[i] + "AM";
         }else{
             hourEl = times[i] + "PM";
         }
 
-        $(hourDiv).text(hourEl);
-        console.log($(hourDiv).text());
+        $(hourDiv).text(hourEl); //Add hours to hour divs
 
-        var descriptionEl = $("<input>");
+        //description elements
+        var descriptionEl = $("<textarea>");
         descriptionEl.attr("class", "description");
         descriptionEl.attr("data-hour", h);
         descriptionEl.attr("id", h + "description");
 
         console.log("data-hour: " + $(hourDiv).attr("data-hour"));
 
+        //Determine if timeblock is past, present or futher based
         if(parseInt($(hourDiv).attr("data-hour")) < currentHour){
+
             $(descriptionEl).attr("class", "description past");
             console.log("Past");
+
         } else if($(hourDiv).attr("data-hour") == currentHour){
+
             $(descriptionEl).attr("class", "description present");
             console.log("Current Hour!");
+
         } else{
+
             $(descriptionEl).attr("class", "description future");
+            console.log("Future");
         }
 
-        timeblockDiv.append(descriptionEl);
-
-        //check to see if there are any storedDescription in localStorage. If so, set them to the empty descriptions array
-        var storedMeetings = JSON.parse(localStorage.getItem("meetings"));
-        if (storedMeetings !== null){
-            meetings = storedMeetings;
-        }
-
-        var item = meetings.find(item => item.hour == h);
-        if (item !== undefined){
-            $(descriptionEl).val(item.description);
-
-
-        }
+        timeblockDiv.append(descriptionEl); //append description element to timeblock div
 
         var saveDiv = $("<div>");
-        saveDiv.attr("class", "save");
+        saveDiv.attr("class", "smallEl");
         $(timeblockDiv).append(saveDiv);
 
         var saveBtn = $("<input>");
@@ -89,7 +85,21 @@ function buildScheduler(){
         saveBtn.attr("data-hour", h);
         $(saveDiv).append(saveBtn);
 
-        startHour++;
+        //Checks to see if there are any meetings in localStorage. Adds the saved descriptions to the description element
+        //Check to see if there are any storedDescription in localStorage. If so, set them to the empty descriptions array
+        var storedMeetings = JSON.parse(localStorage.getItem("meetings"));
+
+        if (storedMeetings !== null){
+            meetings = storedMeetings;
+        }
+
+        var item = meetings.find(item => item.hour == h);
+
+        if (item !== undefined){
+            $(descriptionEl).val(item.description);
+        }
+
+        // startHour++;
         j++;
         h++;
     }
